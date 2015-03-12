31fa7f9f2d3db34568b5ad0e00a0260abd4313cc
xuyuanxiang <447972379@qq.com>
2015-03-12 20:42:50 +0800

    文件目录调整

---
+++ e2e_conf.js
@@ -0,0 +1,17 @@
+/**
+ * @overview
+ *
+ * @version
+ * @author xuyuanxiang
+ * @date 15/1/24
+ * ------------------
+ * @overview
+ * @version
+ * @modifier
+ * @date
+ * ------------------
+ */
+exports.config = {
+    seleniumAddress: 'http://localhost:4444/wd/hub',
+    specs: ['']
+};
